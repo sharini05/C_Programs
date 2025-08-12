41cbc1ef9d922a47fb2588a928b5dc8744fad420
sharini05 <sharini.s123@gmail.com>
2025-08-12 10:44:57 +0530

    Program_11

---
+++ Level_1/Q12.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b;
+    printf("Enter a:");
+    scanf("%d",a);
+    printf("Enter b:");
+    scanf("%d",b);
+    int c=a+b;
+    printf("Addition of two number:%d",c);
+    return 0;
+}
